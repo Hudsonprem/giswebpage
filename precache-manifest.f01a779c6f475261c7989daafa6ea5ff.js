155dd2c7ed742d3d1ab60ed432eeb2ca140148ae
Hudson Daniel <hudsonprem97@gmail.com>
2020-07-30 14:03:55 +0530

    Updates

---
+++ precache-manifest.f01a779c6f475261c7989daafa6ea5ff.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa7b4c59a8bc2d6e169b5e0e81eb7a3",
+    "url": "/giswebpage/index.html"
+  },
+  {
+    "revision": "9d97eb53d4a1b8584fe6",
+    "url": "/giswebpage/static/css/2.c7296f5a.chunk.css"
+  },
+  {
+    "revision": "7544bf79b9a2f2aa802a",
+    "url": "/giswebpage/static/css/main.f80edc8a.chunk.css"
+  },
+  {
+    "revision": "9d97eb53d4a1b8584fe6",
+    "url": "/giswebpage/static/js/2.fcaedf54.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/giswebpage/static/js/2.fcaedf54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7544bf79b9a2f2aa802a",
+    "url": "/giswebpage/static/js/main.6d9f85f8.chunk.js"
+  },
+  {
+    "revision": "915f22a23f3eee1c350d",
+    "url": "/giswebpage/static/js/runtime-main.9d9da81f.js"
+  },
+  {
+    "revision": "49be7c3cb509c49c7559a1c0426e33d6",
+    "url": "/giswebpage/static/media/01_deploy.49be7c3c.svg"
+  },
+  {
+    "revision": "6d3430ab87bdd5f15bee89529b7a9bd4",
+    "url": "/giswebpage/static/media/02_configure.6d3430ab.svg"
+  },
+  {
+    "revision": "04dadaf1608af5fcc6e8b59e15836055",
+    "url": "/giswebpage/static/media/03_analyse.04dadaf1.svg"
+  },
+  {
+    "revision": "3411a5ef0c89495c0e1f41e9b01a2cce",
+    "url": "/giswebpage/static/media/04_role.3411a5ef.svg"
+  },
+  {
+    "revision": "f965660ba093876f606899fc6c67df1e",
+    "url": "/giswebpage/static/media/05_safe.f965660b.svg"
+  },
+  {
+    "revision": "692009cb9771fa7af46fb689b23ef3ba",
+    "url": "/giswebpage/static/media/06_white_label.692009cb.svg"
+  },
+  {
+    "revision": "767068007f0139461532460e605f81c6",
+    "url": "/giswebpage/static/media/Carousel1.76706800.svg"
+  },
+  {
+    "revision": "4b223e8f9261d96c4db44235f9601c54",
+    "url": "/giswebpage/static/media/Carousel2.4b223e8f.svg"
+  },
+  {
+    "revision": "c1dddd136461dc86a1e70c85fb031855",
+    "url": "/giswebpage/static/media/Carousel3.c1dddd13.svg"
+  },
+  {
+    "revision": "c6f89e8a6fde9735844550f86c26ea1f",
+    "url": "/giswebpage/static/media/academic_h.c6f89e8a.svg"
+  },
+  {
+    "revision": "834ecc13e10291b91784bf1bbfe87d95",
+    "url": "/giswebpage/static/media/carousel.834ecc13.svg"
+  },
+  {
+    "revision": "d894823db9ef5893c42877a1f2fff340",
+    "url": "/giswebpage/static/media/enterprises_h.d894823d.svg"
+  },
+  {
+    "revision": "0602a44cc24f7fad1d7daf320eb45c6b",
+    "url": "/giswebpage/static/media/facebook.0602a44c.svg"
+  },
+  {
+    "revision": "489d44169518741efc3bdbeb073f7451",
+    "url": "/giswebpage/static/media/government_h.489d4416.svg"
+  },
+  {
+    "revision": "5aea3f8871db21716a18a35846ca1af0",
+    "url": "/giswebpage/static/media/gwm_web_white.5aea3f88.svg"
+  },
+  {
+    "revision": "542014d6048ed6ba566d08f4573e2100",
+    "url": "/giswebpage/static/media/individual_h.542014d6.svg"
+  },
+  {
+    "revision": "57ac8a3a8a26ae771603aadca4a29925",
+    "url": "/giswebpage/static/media/landing_feature.57ac8a3a.png"
+  },
+  {
+    "revision": "2e020fb5e27bbd6754e31c1ae6fb6b93",
+    "url": "/giswebpage/static/media/landing_page_save_10.2e020fb5.svg"
+  },
+  {
+    "revision": "b92a602ef1c17c20b325e81b188b56c2",
+    "url": "/giswebpage/static/media/ngo_h.b92a602e.svg"
+  },
+  {
+    "revision": "768c1ee656bed03bcd22ce1f672f07ab",
+    "url": "/giswebpage/static/media/pricing_enterprise.768c1ee6.svg"
+  },
+  {
+    "revision": "694a4a70dd6c882e92cb9c6c65e8e7cf",
+    "url": "/giswebpage/static/media/pricing_free.694a4a70.svg"
+  },
+  {
+    "revision": "3628b7227b23a673b321e7d247167cb0",
+    "url": "/giswebpage/static/media/pricing_professional.3628b722.svg"
+  },
+  {
+    "revision": "62bd0d2d908ba6dcc749cbd5cd2bc13c",
+    "url": "/giswebpage/static/media/pricing_standard.62bd0d2d.svg"
+  },
+  {
+    "revision": "3c51972797f4c22714985c96d1d9b3ae",
+    "url": "/giswebpage/static/media/telegram.3c519727.svg"
+  },
+  {
+    "revision": "ef24c412e0ac933a2b5ca45a40b3f2a6",
+    "url": "/giswebpage/static/media/theme.ef24c412.png"
+  },
+  {
+    "revision": "98e9fed66404885bd5ca6876095c0c09",
+    "url": "/giswebpage/static/media/tick.98e9fed6.svg"
+  },
+  {
+    "revision": "9b3576707d6f55a8821035db7c1f17b9",
+    "url": "/giswebpage/static/media/twitter.9b357670.svg"
+  }
+]);
